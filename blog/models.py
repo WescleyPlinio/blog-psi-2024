aa752438f9e92379a19af1426653f6ad079a0b42
Wescley <wescleygaldino06@gmail.com>
2024-09-06 09:33:37 -0300

    adicionando banco de dados básico ao django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=350)
+    subtitulo = models.CharField(max_length=50)
+    conteudo = models.TextField(max_length=2000)
+
