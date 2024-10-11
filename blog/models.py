e371a829273cb8c805be57e877510e23a80079a2
Wescley <wescleygaldino06@gmail.com>
2024-10-11 07:53:50 -0300

    alterações no modelo do banco

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+class Caracteristica(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=350)
+
+
 class Post(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.TextField(max_length=350)
     conteudo = models.TextField(max_length=2000, default='default_value')
-    carac = models.TextField(max_length=250, default='default_value')
+    carac = models.ForeignKey(Caracteristica, on_delete=models.CASCADE)
     imagem = models.ImageField(upload_to='static/imgs')
     dt_postagem = models.DateTimeField(auto_now=True)
 
